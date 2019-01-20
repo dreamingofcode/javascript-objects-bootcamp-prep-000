22f2777b560f639ca603b2e19cade914756125e6
cesar mota <cesarmota@ymail.com>
2019-01-20 03:26:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,3 +7,9 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist['Phil Ochs'] = "Here's to the State of Mississippi"
   return playlist
 }
+var playlist= {artistName,songTitle};
+
+function updatePlaylist(playlist,artistName,songTitle){
+  playlist [artistName]='songTitle';
+  return object.assign({},artistName,songTitle)
+}
