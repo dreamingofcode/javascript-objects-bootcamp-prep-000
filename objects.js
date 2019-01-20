d566ee9079769da5ac8b8807dca4d2c4f17666ba
cesar mota <cesarmota@ymail.com>
2019-01-20 03:28:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ var playlist= {artistName,songTitle};
 function updatePlaylist(playlist,artistName,songTitle){
   playlist [artistName]=songTitle;
   return playlist}
+function updatePlaylist(playlist,artistName,songTitle){
+  playlist[artistName]=songTitle
+  return playlist
+}
