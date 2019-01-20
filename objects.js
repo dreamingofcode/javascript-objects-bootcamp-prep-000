293d233e75719df1b3c2f80eb30beb8f9cab4e3a
cesar mota <cesarmota@ymail.com>
2019-01-20 03:00:22 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,6 @@
+var playlist=new object(artistName,songTitle);
+
+function updatePlaylist([playlist],artistName,songTitle){
+  playlist[artistName]='songTitle'
+  return object.assign({},artistName,songTitle)
+}
