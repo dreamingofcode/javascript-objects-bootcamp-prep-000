2fa1c5b459233ea4d9df12bba3f39759272ae0f3
cesar mota <cesarmota@ymail.com>
2019-01-20 03:52:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -11,3 +11,17 @@ function updatePlaylist(playlist,artistName,songTitle){
   return playlist
 }
 
+var playlist = {
+  artistName: "Slowdive",
+  songTitle: "Alison",
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
